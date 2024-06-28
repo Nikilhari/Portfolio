bbeba6bcac5e1ba2dd711e2d38b30016f3a45c7b
Nikilhari <nikilhari3@gmail.com>
2024-06-28 20:55:20 +0530

    home

--- src/componets/Home/Home.jsx
+++ src/componets/Home/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react'
 import './Home.css'
 import Social from './Social'
 import Data from './Data'
+import ScrollDown from './ScrollDown'
 const Home = () => {
     return (
         <>
             <div className="home section" id="home">
-                <div className="home__container container grid">
-                    <div className="home__content grid">
+                <div className="home__container container">
+                    <div className="home__content">
                         <Social />
                         <div className="home__img"></div>
                         <Data />
                     </div>
+                    <ScrollDown />
                 </div>
             </div>
         </>
