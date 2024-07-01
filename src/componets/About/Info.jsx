0973e2b1bc5c4f64d6f3cee937cf6f46b0cc743d
Nikilhari <nikilhari3@gmail.com>
2024-07-01 22:12:18 +0530

    About 3

--- src/componets/About/Info.jsx
+++ src/componets/About/Info.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Info = () => {
     return (
-        <div>Info</div>
+        <div className="about__info">
+            <div className="about__box">
+                <i class='bx bx-briefcase-alt about__icon'></i>
+                <h3 className="about__title">Completed</h3>
+                <span className="about__subtitle">
+                    3 projects
+                </span>
+            </div>
+            <div className="about__box">
+                <i class='bx bx-support about__icon'></i>
+                <h3 className="about__title">24/7</h3>
+                <span className="about__subtitle">
+                    Support
+                </span>
+            </div>
+        </div>
     )
 }
 
