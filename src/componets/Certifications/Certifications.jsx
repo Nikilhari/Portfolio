ebae5f41c9427c7473a073b75e000a0d9dc1e430
Nikilhari <nikilhari3@gmail.com>
2024-07-20 14:21:43 +0530

    certifications completed

--- src/componets/Certifications/Certifications.jsx
+++ src/componets/Certifications/Certifications.jsx
@@ -1,11 +1,51 @@
 import React from 'react'
+import './Certifications.css'
+import {Data} from './Data.jsx'
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import { Pagination } from 'swiper/modules';
 const Certifications = () => {
   return (
-    <div className="certifications container">
+    <div className="certifications container section" id='certifications'>
          <h2 className="section__title">
             My certifications
         </h2>
+        <Swiper
+        className="certifications__container"
+        loop={true}
+        grabCursor={true}
+        spaceBetween={24}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          576: {
+            slidesPerView: 2,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 48,
+          },
+        }}
+        modules={[Pagination]}
+        >
+            {Data.map(({id,img,title,desc})=>{
+                return(
+                    <SwiperSlide className="certifications__card" key={id}>
+                        <img src={img} alt="" className="certifications__img" />
+                        <h3 className="certifications__name">{title}</h3>
+                        <p className="certifications__desc">
+                            {desc}
+                        </p>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     </div>
   )
 }
---
+++ src/componets/Certifications/Data.jsx
@@ -0,0 +1,30 @@
+import img1 from '../../assets/ui.png'
+import img2 from '../../assets/ml.png'
+import img3 from '../../assets/ml101.png'
+export const Data =[
+{
+    id:1,
+    img:img1,
+    title:"Foundations of User Experience (UX) Design",
+    desc:"--Google(coursera)"
+},
+{
+    id:2,
+    img:img2,
+    title:"Supervised Machine Learning: Regression and Classification",
+    desc:"--Stanford(coursera)"
+},
+{
+    id:3,
+    img:img2,
+    title:"Advanced Learning Algorithms",
+    desc:"--Stanford(coursera)"
+},
+{
+    id:4,
+    img:img3,
+    title:"Machine Learning 101",
+    desc:"--Guvi"
+},
+
+];
