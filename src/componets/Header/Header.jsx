35a38123ea84617e2400a1056ea9a79d51df53b2
Nikilhari <nikilhari3@gmail.com>
2024-07-23 22:17:52 +0530

    header 1

--- src/componets/Header/Header.jsx
+++ src/componets/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Header.css'
 const Header = () => {
     const [toggle , setToggle] = useState(false)
+    const [active,setActive] = useState(0);
     return (
         <div className="header">
             <nav className="nav container">
@@ -9,44 +10,42 @@ const Header = () => {
                 <div className={toggle ? "nav__menu show__menu" : "nav__menu"}>
                     <ul className="nav__list">
                         <li className="nav__item">
-                            <a href="#home" className="nav__link active-link">
+                            <a href="#home" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-estate nav__icon"></i>Home
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#about" className="nav__link">
+                            <a href="#about" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-user nav__icon"></i>About
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#skills" className="nav__link">
+                            <a href="#skills" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-file-alt nav__icon"></i>Skills
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#services" className="nav__link">
+                            <a href="#services" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-briefcase nav__icon">
                                 </i>Services
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#projects" className="nav__link">
+                            <a href="#projects" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-scenery nav__icon">
-
                                 </i> Projects
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#certifications" className="nav__link">
+                            <a href="#certifications" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-scenery nav__icon">
 
                                 </i> Certifications
                             </a>
                         </li>
                         <li className="nav__item">
-                            <a href="#contact" className="nav__link">
+                            <a href="#contact" className={active ==0 ?"nav__link active-link":"nav__link"}>
                                 <i className="uil uil-message nav__icon">
-
                                 </i>Contact
                             </a>
                         </li>
