6ee8c34adc5a052b1d801b7fbdf3a36d5a8847bd
Nikilhari <nikilhari3@gmail.com>
2024-08-12 09:34:45 +0530

    links

--- src/componets/Home/Social.jsx
+++ src/componets/Home/Social.jsx
@@ -4,10 +4,10 @@ const Social = () => {
     return (
         <>
             <div className="home__social">
-                <a href="" className="home__social-icon" target='_blank'>
+                <a href="https://github.com/Nikilhari" className="home__social-icon" target='_blank'>
                     <i className="uil uil-github-alt"></i>
                 </a>
-                <a href="" className="home__social-icon" target='_blank'>
+                <a href="https://www.linkedin.com/in/nikilhari-r-767568259/" className="home__social-icon" target='_blank'>
                     <i className="uil uil-linkedin-alt"></i>
                 </a>
             </div>
