63fcc2ae786e19a38d12826733660ba03c59df1f
Nikilhari <nikilhari3@gmail.com>
2025-02-13 15:50:00 +0530

    resume update

--- src/componets/Skills/Skills.jsx
+++ src/componets/Skills/Skills.jsx
@@ -1,19 +1,19 @@
-import Frontend from './Frontend';
-import Backend from './Backend';
-import './Skills.css'
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+import "./Skills.css";
 const Skills = () => {
-    return (
-        <div className="skills section" id="skills">
-            <h2 className="section__title">
-                Skills
-            </h2>
-            <span className="section__subtitle">My tecnical level</span>
-            <div className="skills__container container">
-                <Frontend />
-                <Backend />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="skills section" id="skills">
+      <h2 className="section__title">Skills</h2>
 
-export default Skills
+      <span className="section__subtitle">My tecnical level</span>
+
+      <div className="skills__container container">
+        <Frontend />
+        <Backend />
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
